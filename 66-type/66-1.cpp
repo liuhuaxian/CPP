d6763bb4e8a7a604ce4b5f6689c8388bc5e4dfa9
yanlele <yanlele@chinaredflag.cn>
2021-02-05 10:43:13 +0800

    66-1-type

---
+++ 66-type/66-1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Base{
+public:
+    virtual string type(){
+        return "Base";
+    }   
+};
+class Derived:public Base{
+public:
+        string type(){
+                return "Derived";    
+        }   
+void print(){
+        cout << "I'm a Derived." << endl;
+    }   
+};
+class Child:public Base{
+public:
+        string type(){
+                return "Child";
+        }   
+};
+void test(Base* b){ 
+    /** 危险的转换方式 */
+    if(b->type()=="Derived"){//if()中的函数不会调用
+                Derived* d = static_cast<Derived*>(b);
+                d->print();
+        }   
+        /*cout << dynamic_cast<Derived*>(b)<< endl;
+ *      0
+        I'm a Derived.
+        0x7ffdd92349c0
+        0
+思考：dynamic_cast<Derived*>(b); 为什么不适用于这里呢？
+dynamic_cast 可以判断是否转换成功，但无法识别指针实际指向的动态类型
+*/
+}
+int main(){
+        Base b;
+        Derived d;
+        Child c;
+        test(&b);
+        test(&d);
+        test(&c);
