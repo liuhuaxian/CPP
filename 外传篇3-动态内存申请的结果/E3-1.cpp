4ef1b2dcd20ce531b0c1672d15d51e152e573f3a
yanlele <464516186@qq.com>
2021-02-17 17:07:46 +0800

    2021年02月17日 17:07:45 push 外传篇3-动态内存申请的结果/

--- 外传篇3-动态内存申请的结果/E3-1.cpp
+++ 外传篇3-动态内存申请的结果/E3-1.cpp
@@ -54,17 +54,23 @@ public:
     }
 };
 
-void my_new_handler()
+void my_new_handler()//定义一个自己的new_handle()函数
 {
     cout << "void my_new_handler()" << endl;
 }
-
-void ex_func_1()
-{
+/*
+系统提示出现了std::bad_alloc异常。
+已知：在使用new分配内存空间时，内存空间不够时就会抛出该异常。
+解决：在程序中加入delete和clear及时释放内存，
+并且对原始数据进行分割处理。
+new_handler是预定义的函数指针，类型是
+*/
+void ex_func_1()//证明new_handle()全局函数的存在
+{//set_new_handler()返回值是原来的处理函数
     new_handler func = set_new_handler(my_new_handler);
     
     try
-    {
+    {//默认情况下是没有处理函数的,显然func为空
         cout << "func = " << func << endl;
         
         if( func )
@@ -72,6 +78,8 @@ void ex_func_1()
             func();
         }
     }
+	//这里是为了证明默认的new_handler()处理函数确实是要抛出
+	//bad_alloc异常的
     catch(const bad_alloc&)
     {
         cout << "catch(const bad_alloc&)" << endl;
@@ -122,7 +130,8 @@ void ex_func_3()
 
 int main(int argc, char *argv[])
 {
-    // ex_func_1();
+	//BCC编译器是有默认处理函数的,1说明不同的编译器new失败的行为是有区别的
+    // ex_func_1();输出:func=0 说明了new_handler()并没有全局的处理函数
     // ex_func_2();
     // ex_func_3();
     
